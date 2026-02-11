9a7b69ac2f69661789690d0a3e26f7fe5a45145e
m4dmood <r.maddaloni494@gmail.com>
2026-02-11 21:39:14 +0100

    fix error log db

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -18,12 +18,14 @@ func Connect(ctx context.Context) (*DB, error) {
 	// Connection pool
 	pool, err := pgxpool.New(ctx, connStr)
 	if err != nil {
-		return nil, fmt.Errorf("[DATABASE] Error while creating pool -> %w", err)
+		fmt.Printf("[DATABASE] Error while creating pool -> %v", err)
+		return nil, err
 	}
 
 	// Test connection
 	if err := pool.Ping(ctx); err != nil {
-		return nil, fmt.Errorf("[DATABASE] Ping failed -> %w", err)
+		fmt.Printf("[DATABASE] Ping failed -> %v", err)
+		return nil, err
 	}
 
 	fmt.Println("[DATABASE] Database connected successfully! (Pool)")
@@ -32,7 +34,7 @@ func Connect(ctx context.Context) (*DB, error) {
 
 func (db *DB) InsertWithFallback(ctx context.Context, entry parser.RegistryEntry) error {
 
-	query := `ÌNSERT INTO registry (ts, device_id, event_type, val, event_message, level)
+	query := `INSERT INTO registry (ts, device_id, event_type, val, event_message, level)
 				VALUES ($1, $2, $3, $4, $5, $6)`
 
 	_, err := db.Pool.Exec(ctx, query,
@@ -45,13 +47,19 @@ func (db *DB) InsertWithFallback(ctx context.Context, entry parser.RegistryEntry
 	)
 
 	if err != nil {
+		fmt.Printf("[DATABASE] Error while inserting data, moving record to error log -> %v\n", err)
 		db.logErrorToTable(ctx, entry.ToRawString(), err.Error())
+		return err
 	}
 
+	fmt.Printf("[DATABASE] Logged event -> %v\n", entry)
 	return nil
 }
 
 func (db *DB) logErrorToTable(ctx context.Context, rawData string, reason string) {
+	fmt.Printf("[DATABASE] Moving record to error log -> %s\n", rawData)
+	fmt.Printf("[DATABASE] Invalid record due to %s\n", reason)
+
 	query := `INSERT INTO registry_errors (raw_data, error_message) VALUES ($1, $2)`
 	db.Pool.Exec(ctx, query, rawData, reason)
 }
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/m4dmood/black-box/internal/database"
 	"github.com/m4dmood/black-box/internal/parser"
@@ -15,15 +14,15 @@ import (
 
 func main() {
 	// 1. Context creation for managing timeouts and cancellations
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	ctx := context.Background()
+	//defer cancel()
 
-	fmt.Println("[BLACK_BOX] Starting Black-Box...")
+	fmt.Println("[MAIN] Starting Black-Box...")
 
 	// 2. Database connection
 	db, err := database.Connect(ctx)
 	if err != nil {
-		fmt.Printf("[BLACK_BOX] Critical error during database startup: %v\n", err)
+		fmt.Printf("[MAIN] Critical error during database startup: %v\n", err)
 		os.Exit(1)
 	}
 	defer db.Close()
@@ -33,21 +32,29 @@ func main() {
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
-	fmt.Println("[BLACK_BOX] System is ready. Waiting for CSV files in the 'inbox' folder...")
-	fmt.Println("[BLACK_BOX] Press Ctrl+C to stop the service.")
+	fmt.Println("[MAIN] System is ready. Waiting for CSV files in the 'inbox' folder...")
+	fmt.Println("[MAIN] Press Ctrl+C to stop the service.")
 
 	// Start watcher and pass incoming data to channel
 	fileQueue := make(chan string, 100)
-	fmt.Println("[BLACK_BOX] Monitoring directory /inbox...")
+	fmt.Println("[MAIN] Monitoring directory /inbox...")
 	watcher.Watch("./inbox", fileQueue)
 
 	// 4. Elaboration loop: process incoming data from channel
 	go func() {
 		for filePath := range fileQueue {
-			fmt.Printf("[BLACK_BOX] File process started: %s\n", filePath)
-			_, err := parser.ProcessFile(filePath)
+			fmt.Printf("[MAIN] File process started: %s\n", filePath)
+			entries, err := parser.ProcessFile(filePath)
+
 			if err != nil {
-				fmt.Printf("[BLACK_BOX] Error while parsing file %s: %v\n", filePath, err)
+				fmt.Printf("[MAIN] Error while parsing file %s: %v\n", filePath, err)
+			}
+
+			for _, entry := range entries {
+				err := db.InsertWithFallback(ctx, entry)
+				if err != nil {
+					fmt.Printf("[MAIN] Error while inserting entry into database: %v\n", err)
+				}
 			}
 
 			os.Remove(filePath)
@@ -57,8 +64,8 @@ func main() {
 	// Application will keep running until it receives an interrupt signal
 	<-stop
 
-	fmt.Println("\n[BLACK_BOX] Stop signal received. Shutting down gracefully...")
+	fmt.Println("\n[MAIN] Stop signal received. Shutting down gracefully...")
 
 	// Successful shutdown message
-	fmt.Println("[BLACK_BOX] Black-Box graceful shutdown completed successfully!")
+	fmt.Println("[MAIN] Black-Box graceful shutdown completed successfully!")
 }
